4de68cac83b33f1c6117f932b2069378455cf469
aaajug <augallardo@up.edu.ph>
2021-05-09 20:20:56 +0800

    schema update in establisments

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_05_07_140418) do
 
   create_table "establishments", force: :cascade do |t|
-    t.string "name"
+    t.string "estname"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
